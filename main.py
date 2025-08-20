355c1f87814ca0478da5d75ec9f33a80dce644d3
Bryce Turner <74370313+brycecturner@users.noreply.github.com>
2025-08-19 22:06:00 -0400

    have the hypothesis generator working fully and saving output to machine-and-human readable format. Next step is to create the testing framworks.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import asyncio
 from dotenv import load_dotenv  # type: ignore
+import json
 
 from crew.run_parallel_crews import run_parallel_crews
 from utils.utils import join_outputs_as_json
@@ -37,37 +38,37 @@ async def main():
     # 1. Run research agents in parallel
     results = await run_parallel_crews([
         (create_research_fed_policy_task(), create_fed_policy_research_agent()),
-        # (create_research_banking_risk_task(), create_banking_risk_research_agent()),
-        # (create_research_global_capital_flows_task(), create_global_capital_flows_research_agent()),
-        # (create_research_fiscal_policy_task(), create_fiscal_policy_research_agent()),
-        # (create_research_macro_growth_task(), create_macro_growth_research_agent()),
-        # (create_research_macro_inflation_task(), create_macro_inflation_research_agent()),
+        (create_research_banking_risk_task(), create_banking_risk_research_agent()),
+        (create_research_global_capital_flows_task(), create_global_capital_flows_research_agent()),
+        (create_research_fiscal_policy_task(), create_fiscal_policy_research_agent()),
+        (create_research_macro_growth_task(), create_macro_growth_research_agent()),
+        (create_research_macro_inflation_task(), create_macro_inflation_research_agent()),
     ])
-
-    # # 2. Print outputs for visibility
-    # for (task, agent, output) in results:
-    #     print("\n-----")
-    #     print(f"{agent}")
-    #     print(output)
 
     # 3. Save research results JSON
     result_json = join_outputs_as_json(results)
     with open("results/research_summary.json", "w") as f:
         f.write(result_json)
-
+    
     # 4. Run trader agent on the research results
-    # trader_task = create_trader_task(result_json)
-    # trader_agent = create_trader_agent()
     trader_output = await run_parallel_crews([
         (create_trader_task(result_json), create_trader_agent()),
     ])
+    _, _, crew_out = trader_output[0]
 
-    # 5. Save trader hypotheses
+    # # Save to a human-readable JSON file
     with open("results/trader_hypotheses.json", "w") as f:
-        f.write(join_outputs_as_json(trader_output))
+        if isinstance(crew_out.raw, str):
+            try:
+                json.dump(json.loads(crew_out.raw), f, indent=2)
+            except Exception:
+                f.write(crew_out.raw)
+        else:
+            json.dump(crew_out.raw, f, indent=2)
 
-    print("\n===== Trader Hypotheses =====")
-    print(trader_output)
+
+    # print("\n===== Trader Hypotheses =====")
+    # print(final_trader_json)
 
 
 if __name__ == "__main__":
